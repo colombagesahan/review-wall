6c9b51ffa1b81f822456a8fbecf96e71fed14d89
colombagesahan <colombagesahan@gmail.com>
2025-12-04 13:49:40 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
-  // --- PASTE YOUR KEYS BELOW ---
-  apiKey: "AIzaSy...", 
+  apiKey: "AIzaSyBUjzMFao9BS3uXBOW3qYrLVqHaGn8qIk4",
   authDomain: "onlineshop-30cd1.firebaseapp.com",
   projectId: "onlineshop-30cd1",
   storageBucket: "onlineshop-30cd1.firebasestorage.app",
   messagingSenderId: "818252574868",
-  appId: "1:818252574868:web:..."
+  appId: "1:818252574868:web:8dd36825db589a886cc481",
+  measurementId: "G-6YLGGH2NDD"
 };
 
 const app = initializeApp(firebaseConfig);
