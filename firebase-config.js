d92bfd71a115a593bfc6c8e2b889e3665cb66371
colombagesahan <colombagesahan@gmail.com>
2025-12-05 19:36:31 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
-
+// firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyBUjzMFao9BS3uXBOW3qYrLVqHaGn8qIk4",
+  apiKey: "AIzaSyBUjzMFao9BS3uXBOW3qYrLVqHaGn8qIk4", // REPLACE WITH YOUR REAL KEY
   authDomain: "onlineshop-30cd1.firebaseapp.com",
   projectId: "onlineshop-30cd1",
   storageBucket: "onlineshop-30cd1.firebasestorage.app",
   messagingSenderId: "818252574868",
-  appId: "1:818252574868:web:8dd36825db589a886cc481",
-  measurementId: "G-6YLGGH2NDD"
+  appId: "1:818252574868:web:8dd36825db589a886cc481"
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-export { auth, db, storage };
+// Initialize
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
