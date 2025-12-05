e68eeb17707629cc782e9f1603e81fa5d0724854
colombagesahan <colombagesahan@gmail.com>
2025-12-05 19:52:28 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,11 @@
-// firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyBUjzMFao9BS3uXBOW3qYrLVqHaGn8qIk4", // REPLACE WITH YOUR REAL KEY
+  apiKey: "AIzaSyBUjzMFao9BS3uXBOW3qYrLVqHaGn8qIk4", // REPLACE WITH REAL KEY
   authDomain: "onlineshop-30cd1.firebaseapp.com",
   projectId: "onlineshop-30cd1",
   storageBucket: "onlineshop-30cd1.firebasestorage.app",
   messagingSenderId: "818252574868",
   appId: "1:818252574868:web:8dd36825db589a886cc481"
 };
-
-// Initialize
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
