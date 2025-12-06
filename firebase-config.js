82741c245c94ac9258525d812c29821a654e07e2
colombagesahan <colombagesahan@gmail.com>
2025-12-06 11:56:51 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,20 @@
+// firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyBUjzMFao9BS3uXBOW3qYrLVqHaGn8qIk4", // REPLACE WITH REAL KEY
+  apiKey: "AIzaSyBUjzMFao9BS3uXBOW3qYrLVqHaGn8qIk4", // Your Real Key
   authDomain: "onlineshop-30cd1.firebaseapp.com",
   projectId: "onlineshop-30cd1",
   storageBucket: "onlineshop-30cd1.firebasestorage.app",
   messagingSenderId: "818252574868",
   appId: "1:818252574868:web:8dd36825db589a886cc481"
 };
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+// Attach to window so all pages can see them
+window.auth = firebase.auth();
+window.db = firebase.firestore();
+
+console.log("Firebase Connected Successfully");
